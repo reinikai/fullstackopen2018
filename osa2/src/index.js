03809ba840e41baad6d193a6bbc184747c01f453
Jani Reinikainen <jani@noviaria.com>
2018-09-21 14:31:11 +0300

    Implement 2.5 erillinen moduuli

--- osa2/src/index.js
+++ osa2/src/index.js
@@ -1,52 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Kurssi = ({ kurssi }) => {
-    return (
-        <div>
-            <Otsikko kurssi={kurssi} />
-            <Sisalto kurssi={kurssi} />
-            <Yhteensa kurssi={kurssi} />
-        </div>
-    )
-}
-
-const Otsikko = (props) => {
-    return (
-        <div>
-            <h1>{props.kurssi.nimi}</h1>
-        </div>
-    )
-}
-
-
-const Osa = (props) => {
-    return (
-        <div>
-            <p>{props.name} {props.tehtavia}</p>
-        </div>
-    )
-}
-
-const Sisalto = ({kurssi}) => {
-    return (
-        <div>
-            {kurssi.osat.map(osa=><Osa key={osa.id} name={osa.nimi} tehtavia={osa.tehtavia}/>)}
-        </div>
-    )
-}
-
-const Yhteensa = ({kurssi}) => {
-
-    const reducer = (accumulator, currentValue) => accumulator + parseInt(currentValue.tehtavia, 0)
-    const yht = kurssi.osat.reduce(reducer, 0);
-
-    return (
-        <div>
-            <p>yhteensä {yht} tehtävää</p>
-        </div>
-    )
-}
+import Kurssi from './components/Kurssi'
 
 const App = () => {
     const kurssit = [
